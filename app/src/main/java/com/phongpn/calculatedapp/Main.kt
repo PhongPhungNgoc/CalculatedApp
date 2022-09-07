4b8c2f39c85696d18b3a0777db66c3fff74eaa3b
PhongPN <phungngocphongit@gmail.com>
2022-09-07 22:42:24 +0700

    Main Project

---
+++ app/src/main/java/com/phongpn/calculatedapp/Main.kt
@@ -0,0 +1,12 @@
+package com.phongpn.calculatedapp
+
+import com.phongpn.calculatedlibrary.add
+import com.phongpn.calculatedlibrary.multiply
+
+
+//import com.phongpn.calculatedlibrary.add
+//import com.phongpn.calculatedlibrary.multiply
+
+fun main() {
+    print("Result: ${add(3,4)} - Multi: ${multiply(3,4)}")
+}
